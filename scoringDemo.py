56dced96977b6d53a57660b9a8124389eb04b062
Havokpalooza <123006818+Havokpalooza@users.noreply.github.com>
2023-02-10 12:32:46 -0500

    Update scoringDemo.py
    Added Twos

--- scoringDemo.py
+++ scoringDemo.py
@@ -9,10 +9,23 @@ class ScoreMachine:
                 total += item
         return total
     
+    def twos(self,rolls):
+        total = 0
+        for item in rolls:
+            it item == 2:
+                total += item
+        return total
+    
 def testAces():
     x = ScoreMachine()
     assert 5 == x.aces([1,1,1,1,1])
     assert 4 == x.aces([1,1,1,1])
     
     print("Aces tests passed!")
+    
+def testTwos():
+    x = ScoreMachine()
+    assert 0 == x.aces([1,3,4,1,5])
+    assert 3 == x.aces([2,1,2,3,2])
+    assert 5 == x.aces([2,2,2,2,2])
     
